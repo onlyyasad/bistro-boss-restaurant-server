f47b0156e3fe226d0c688cf95c580093c8a0de9b
onlyyasad <onlyyasad@gmail.com>
2023-05-29 03:39:54 +0600

    api generated for cart items

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
     const menuCollection = client.db("bistroBossDB").collection("menu");
     const reviewCollection = client.db("bistroBossDB").collection("reviews");
+    const cartsCollection = client.db("bistroBossDB").collection("carts");
 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
@@ -40,6 +41,32 @@ async function run() {
         const result = await menuCollection.find(query).toArray();
         res.send(result)
     })
+
+    // Cart API :
+
+    // app.get('/carts', async(req, res) =>{
+    //   const result = await cartsCollection.find().toArray();
+    //   res.send(result)
+    // })
+
+    app.get('/carts', async(req, res) =>{
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = { email: email};
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/carts', async(req, res) =>{
+      const item = req.body;
+      console.log(item);
+      const result = await cartsCollection.insertOne(item);
+      res.send(result)
+    })
+
+    // Review API :
 
     app.get('/reviews', async(req, res) => {
       const result = await reviewCollection.find().toArray();
